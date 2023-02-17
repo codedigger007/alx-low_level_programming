6b0757bc38197a0c74953ac8fbafd359360a3ad2
elochukwu <elochukwuuzoeto@gmail.com>
2023-02-17 11:24:27 -0800

    no 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - base 16
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+	int hex;
+
+	for (num = 48; num < 58; n++)
+		putchar(num);
+	for (hex = 'a'; hex <= 'f'; hex++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
