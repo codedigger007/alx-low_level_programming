ac566415c48d35353e248eea34c8e4f7eb0e5dc2
elochukwu <elochukwuuzoeto@gmail.com>
2023-02-17 11:08:35 -0800

    no 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - single digits
+ * Return: 0 always
+ */
+int main(0)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	putchar("\n");
+	return (0);
+}
